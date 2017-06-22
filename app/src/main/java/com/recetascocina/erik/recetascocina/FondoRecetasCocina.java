483b3c8d0b00a583945db41f2823cf51ac7c4c17
Carlos Espacios <carlspace@hotmail.com>
2017-06-22 10:50:16 +0100

    Initial commit

---
+++ app/src/main/java/com/recetascocina/erik/recetascocina/FondoRecetasCocina.java
@@ -0,0 +1,13 @@
+package com.recetascocina.erik.recetascocina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FondoRecetasCocina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fondo_recetas_cocina);
+    }
+}
